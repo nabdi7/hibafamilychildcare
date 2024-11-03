cb4705e13e6cf9156d3a4e798df313b16666886b
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-02 22:48:27 -0700

    updated og and favicon

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -9,47 +9,38 @@ const Gallery = () => {
     {
       src: "/img1.jpg",
       alt: "Children playing",
-      caption: "Creative Play Time",
     },
     {
       src: "/img2.jpg",
       alt: "Art activity",
-      caption: "Art & Expression",
     },
     {
       src: "/img3.jpg",
       alt: "Outdoor activities",
-      caption: "Outdoor Adventures",
     },
     {
       src: "/img8.jpg",
       alt: "Music & Dance",
-      caption: "Music & Movement",
     },
     {
       src: "/img5.jpg",
       alt: "Group activities",
-      caption: "Social Development",
     },
     {
       src: "/img6.jpg",
       alt: "Story time",
-      caption: "Story Time Magic",
     },
     {
       src: "/img7.jpg",
       alt: "Science experiments",
-      caption: "Science & Discovery",
     },
     {
       src: "/img9.jpg",
       alt: "Learning activities",
-      caption: "Learning Through Play",
     },
     {
       src: "/img10.jpg",
       alt: "Music & Dance",
-      caption: "Music & Movement",
     },
   ];
 
@@ -91,13 +82,7 @@ const Gallery = () => {
                 alt={image.alt}
                 className="w-full h-full object-cover rounded-xl transition-transform group-hover:scale-105"
               />
-              <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity rounded-xl">
-                <div className="absolute bottom-4 left-4 right-4">
-                  <p className="text-white font-medium text-lg">
-                    {image.caption}
-                  </p>
-                </div>
-              </div>
+              
               {/* Decorative stars */}
               {index % 3 === 0 && (
                 <div className="absolute -top-2 -right-2 bg-green-400 p-2 rounded-full">
