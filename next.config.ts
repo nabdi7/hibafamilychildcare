c01bd0dac816e7fa176536f18a1a4c092c165314
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-03 13:17:20 -0800

    updated components

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {unoptimized: true},
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "res.cloudinary.com",
-  //       port: "",
-  //     },
-  //   ],
-  // },
 };
 
 export default nextConfig;
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,36 +1,42 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://najahfamilychildcare.com',
             lastModified: new Date(),
+            changeFrequency: 'yearly',
             priority: 1,
         },
         {
             url: 'https://najahfamilychildcare.com/about',
             lastModified: new Date(),
+            changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
             url: 'https://najahfamilychildcare.com/programs',
             lastModified: new Date(),
-            priority: 0.8,
+            changeFrequency: 'weekly',
+            priority: 0.5,
         },
         {
             url: 'https://najahfamilychildcare.com/contact',
             lastModified: new Date(),
-            priority: 0.8,
+            changeFrequency: 'weekly',
+            priority: 0.5,
         },
         {
             url: 'https://najahfamilychildcare.com/gallery',
             lastModified: new Date(),
-            priority: 0.8,
+            changeFrequency: 'weekly',
+            priority: 0.5,
         },
         {
             url: 'https://najahfamilychildcare.com/testimonials',
             lastModified: new Date(),
-            priority: 0.8,
+            changeFrequency: 'weekly',
+            priority: 0.5,
         },
     ]
 }
