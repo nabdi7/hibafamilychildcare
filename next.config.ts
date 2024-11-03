a16546dfc817326bc197d4544380dc5f1c0c031d
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-02 22:49:45 -0700

    updated og and favicon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  images: {unoptimized: true},
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "res.cloudinary.com",
+  //       port: "",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
